e65f22eccca708df6a298e109ceef3365c9c4326
Marcin Spasinski <marcin7789@gmail.com>
2020-04-21 21:34:43 +0200

    remove api to be on its own repo as I should do a long time ago

--- Api-RPG/RPG-Api/Domain/Services/Security/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RPG.Api.Domain.Services.Security
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool PasswordMatches(string providedPassword, string passwordHash);
-    }
-}
